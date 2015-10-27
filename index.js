e23b6692b1bc2ee5af0ad4d926312e147a5d0c4e
Evan Lovely <evan@evanlovely.com>
2015-10-27 15:48:11 -0700

    setting up distributed dependencies and ability to load grunt tasks from dependencies

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt, config, tasks) {
   "use strict";
+  // Wrap Grunt's loadNpmTasks() function to allow loading Grunt task modules
+  // that are dependencies of Grunt Drupal Tasks.
+  // Hat tip: Joe Turgeon via https://github.com/phase2/grunt-drupal-tasks/blob/master/bootstrap.js#L12
+  grunt._loadNpmTasks = grunt.loadNpmTasks;
+  grunt.loadNpmTasks = function (mod) {
+    var internalMod = grunt.file.exists(__dirname, 'node_modules', mod);
+    if (internalMod) {
+      var pathOrig = process.cwd();
+      process.chdir(__dirname);
+    }
+    grunt._loadNpmTasks(mod);
+    if (internalMod) {
+      process.chdir(pathOrig);
+    }
+  };
+
   if (config.features.icons) {
     require('./lib/icons.js')(grunt, config, tasks);
   }
