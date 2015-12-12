b0362fcd7ee78618e6e2cd5e5811019af6eee1a6
Evan Lovely <evan@evanlovely.com>
2015-12-11 18:43:24 -0800

    scss file rename

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = function (grunt, config, tasks) {
   }
   require('./lib/js.js')(grunt, config, tasks);
   if (config.features.scss) {
-    require('./lib/libsass.js')(grunt, config, tasks);
+    require('./lib/scss.js')(grunt, config, tasks);
   }
   if (config.features.patternLab) {
     require('./lib/pattern-lab.js')(grunt, config, tasks);
