c58f96668b917c74bbe35bf8982fb9e4d0687526
Christopher Bloom <bloomcb@gmail.com>
2016-10-06 13:15:57 -0700

    Upgrading all JS to ES6
    Feature/es awesome (#16)
    
    * enabling airbnb eslint standards
    
    * AirBnB lint pattern-lab--php-twig.js
    
    * ES6ify js.js
    
    * ES6ify icons.js
    
    * ES6ify drupal.js, left in console statement, let's see if we can take it out
    
    * ES6ify css.js
    
    * ES6ify core.js
    
    * ES6ify browser-sync.js
    
    * ES6ify index.js - NOTE: fundamental changes have to be made here
    
    * setting strict mode
    
    * turning of requirements of strict mode in eslint
    
    * fixing css compile exiting properly when error
    
    * cleanup warnings and improve drupal cc
    
    * adding eslint comments to disable certain rules
    
    * refactoring icon template approach (#17)
    
    * refactoring icon template approach
    
    * cleaning up lint errors in icons.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// rule reference: http://eslint.org/docs/rules
+// individual rule reference: http://eslint.org/docs/rules/NAME-OF-RULE
+module.exports = {
+  extends: "airbnb",
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    strict: [0],
+  },
+};
